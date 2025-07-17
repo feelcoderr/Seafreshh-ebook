626f49a02569094b39fb37f39094021e9afd3ff1
Dipti Pariya <diptipariya@gmail.com>
2025-07-17 18:23:04 +0530

    fix: console in dev

--- src/app/api/create-order/route.js
+++ src/app/api/create-order/route.js
@@ -15,7 +15,10 @@ export async function POST(request) {
       key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_SECRET_KEY,
     });
-
+    console.log(
+      "process.env.NEXT_PUBLIC_EBOOK_PRICE",
+      process.env.NEXT_PUBLIC_EBOOK_PRICE
+    );
     // Create order
     const order = await razorpay.orders.create({
       amount: process.env.NEXT_PUBLIC_EBOOK_PRICE, // in paisa
