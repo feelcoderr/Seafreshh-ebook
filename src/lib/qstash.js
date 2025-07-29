46b5cf6b96daa8641571a101379e348111a1f5ed
Dipti Pariya <diptipariya@gmail.com>
2025-07-29 19:40:06 +0530

    refactor: import issue

--- src/lib/qstash.js
+++ src/lib/qstash.js
@@ -1,6 +1,6 @@
 // lib/qstash.js
-import { QStashClient } from "@upstash/qstash";
 
+import QStashClient from "@upstash/qstash";
 export const qstash = new QStashClient({
   token: process.env.QSTASH_TOKEN,
 });
